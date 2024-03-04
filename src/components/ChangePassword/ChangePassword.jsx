b040ad428ca235f2826d4a25d5c4b717c3d4c42d
Carlos Duarte <carlosduarte.1@hotmail.com>
2024-03-04 12:27:06 -0700

    feat: change password, delete account

--- src/components/ChangePassword/ChangePassword.jsx
+++ src/components/ChangePassword/ChangePassword.jsx
@@ -1,23 +1,71 @@
 import { PasswordTextField } from "../PasswordTextField/PasswordTextField";
 import Button from "@mui/material/Button";
 import "./ChangePassword.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { URL_USER_CHANGE_PASSWORD, AUTHORIZATION_TOKEN } from "../../shared/constants";
+import toast, { Toaster } from "react-hot-toast";
 
 export const ChangePassword = () => {
-    const [currentPassword, setCurrentPassword] = useState("");
-    const [newPassword, setNewPassword] = useState("");
-    const [confirmedPassword, setConfirmedPassword] = useState("");
+    const [changePasswordDto, setChangePasswordDto] = useState({
+        usernameOrEmail: "",
+        oldPassword: "",
+        newPassword: "",
+        confirmedPassword: ""
+    });
+
+    useEffect(() => {
+        const currentUser = JSON.parse(localStorage.getItem("user"));
+        setChangePasswordDto({
+            ...changePasswordDto,
+            usernameOrEmail: currentUser.username
+        });
+    }, []);
 
     const currentPasswordChanged = (event) => {
-        setCurrentPassword(event.target.value);
+        setChangePasswordDto({
+            ...changePasswordDto,
+            oldPassword: event.target.value
+        });
     }
 
     const newPasswordChanged = (event) => {
-        setNewPassword(event.target.value);
+        setChangePasswordDto({
+            ...changePasswordDto,
+            newPassword: event.target.value
+        });
     }
 
     const confirmedPasswordChanged = (event) => {
-        setConfirmedPassword(event.target.value);
+        setChangePasswordDto({
+            ...changePasswordDto,
+            confirmedPassword: event.target.value
+        });
+    }
+
+    const fetchUpdate = async () => {
+        await fetch(URL_USER_CHANGE_PASSWORD, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": AUTHORIZATION_TOKEN
+            },
+            body: JSON.stringify(changePasswordDto)
+        }).then(response => {
+            if (!response.ok) {
+                return response.text().then(errorText => {
+                    throw new Error(errorText);
+                });
+            }
+            return response.json();
+        }).then(data => {
+            toast.success("Password changed successfully!");
+        }).catch(errorObject => {
+            toast.error(errorObject.message.substring(18, errorObject.message.indexOf("!") + 1));
+        });
+    }
+
+    const handleChangePassword = async () => {
+        await fetchUpdate();
     }
 
     return (
@@ -33,10 +81,11 @@ export const ChangePassword = () => {
                 <PasswordTextField 
                     passwordLabel="Confirmed password"
                     handlePassword={confirmedPasswordChanged} /><br /><br />
-                <Button className="change-password-save-button">
+                <Button className="change-password-save-button" onClick={handleChangePassword}>
                     <i className="material-symbols-outlined">save</i>Save
                 </Button>
-            </form>            
+            </form>       
+            <Toaster />     
         </>
     );
 }
--- src/components/DeleteAccount/DeleteAccount.jsx
+++ src/components/DeleteAccount/DeleteAccount.jsx
@@ -3,22 +3,72 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { PasswordTextField } from "../PasswordTextField/PasswordTextField";
 import { useState } from "react";
+import { URL_ACCOUNT_DELETE, AUTHORIZATION_TOKEN } from "../../shared/constants";
+import toast, { Toaster } from "react-hot-toast";
+import { changeUser } from "../../store/userSlice";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 export const DeleteAccount = () => {
-    const [usernameOrEmail, setUsernameOrEmail] = useState("");
-    const [currentPassword, setCurrentPassword] = useState("");
-    const [confirmedPassword, setConfirmedPassword] = useState("");
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
+    const [deleteAccountDto, setDeleteAccountDto] = useState({
+        usernameOrEmail: "",
+        currentPassword: "",
+        confirmedPassword: ""
+    });
+    
     const usernameChanged = (event) => {
-        setUsernameOrEmail(event.target.value);
+        setDeleteAccountDto({
+            ...deleteAccountDto,
+            usernameOrEmail: event.target.value
+        });
     }
 
     const currentPasswordChanged = (event) => {
-        setCurrentPassword(event.target.value);
+        setDeleteAccountDto({
+            ...deleteAccountDto,
+            currentPassword: event.target.value
+        });
     }
 
     const confirmedPasswordChanged = (event) => {
-        setConfirmedPassword(event.target.value);
+        setDeleteAccountDto({
+            ...deleteAccountDto,
+            confirmedPassword: event.target.value
+        });
+    }
+
+    const fetchUpdate = async () => {
+        await fetch(URL_ACCOUNT_DELETE, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": AUTHORIZATION_TOKEN
+            },
+            body: JSON.stringify(deleteAccountDto)
+        }).then(response => {
+            if (!response.ok) {
+                return response.text().then(errorText => {
+                    throw new Error(errorText);
+                });
+            }
+            return response.json();
+        }).then(data => {
+            toast.success("Your account has beed deleted successfully!");
+            dispatch(changeUser({
+                loggedin: false,
+                user: null
+            }))
+            navigate("/login");
+        }).catch(errorObject => {
+            toast.error(errorObject.message.substring(18, errorObject.message.indexOf("!") + 1));
+        });
+    }
+
+    const handleDeleteAccount = async () => {
+        await fetchUpdate();
     }
 
     return (
@@ -36,10 +86,11 @@ export const DeleteAccount = () => {
                 <PasswordTextField 
                     passwordLabel="Confirmed password"
                     handlePassword={confirmedPasswordChanged} /><br /><br />
-                <Button className="delete-account-delete-button">
+                <Button className="delete-account-delete-button" onClick={handleDeleteAccount}>
                     <i className="material-symbols-outlined">person_remove</i>Delete
                 </Button>
             </form>
+            <Toaster />
         </>
     );
 }
